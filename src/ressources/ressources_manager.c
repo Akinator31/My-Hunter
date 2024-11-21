2941081eeed489fcae2075ef09fc8b45f0baf09b
Pavel <pavel.de-wavrechin@epitech.eu>
2024-11-21 18:13:04 +0100

    Refactor: Remove useless variable's declarations

--- src/ressources/ressources_manager.c
+++ src/ressources/ressources_manager.c
@@ -22,17 +22,12 @@ void destroy_ressources(ressource_manager_t *ressources)
 ressource_manager_t *create_ressources(void)
 {
     ressource_manager_t *ressources = malloc(sizeof(ressource_manager_t));
-    sfTexture *background = sfTexture_createFromFile("assets/images/main_menu/bg.png", &AREA(1920, 1080));
-    sfTexture *play_button = sfTexture_createFromFile("assets/images/main_menu/buttons/start_button.png", &AREA(300, 151));
-    sfTexture *play_button_hover = sfTexture_createFromFile("assets/images/main_menu/buttons/hover/start_button_hover.png", &AREA(300, 151));
-    sfTexture *quit_button = sfTexture_createFromFile("assets/images/main_menu/buttons/quit_button.png", &AREA(154, 151));
-    sfTexture *quit_button_hover = sfTexture_createFromFile("assets/images/main_menu/buttons/hover/quit_button_hover_1.png", &AREA(161, 151));
 
-    ressources->background = background;
-    ressources->play_button = play_button;
-    ressources->play_button_hover = play_button_hover;
-    ressources->quit_button = quit_button;
-    ressources->quit_button_hover = quit_button_hover;
+    ressources->background = sfTexture_createFromFile("assets/images/main_menu/bg.png", &AREA(1920, 1080));
+    ressources->play_button = sfTexture_createFromFile("assets/images/main_menu/buttons/start_button.png", &AREA(300, 151));
+    ressources->play_button_hover = sfTexture_createFromFile("assets/images/main_menu/buttons/hover/start_button_hover.png", &AREA(300, 151));
+    ressources->quit_button = sfTexture_createFromFile("assets/images/main_menu/buttons/quit_button.png", &AREA(154, 151));
+    ressources->quit_button_hover = sfTexture_createFromFile("assets/images/main_menu/buttons/hover/quit_button_hover_1.png", &AREA(161, 151));
     ressources->destroy_ressources = &destroy_ressources;
     return ressources;
 }
